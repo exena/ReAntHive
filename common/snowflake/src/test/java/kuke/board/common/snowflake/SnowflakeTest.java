f4e2045f5d5e502d7a2bb80e2faea48be9e82e84
exena <exena@naver.com>
2026-01-07 18:19:55 +0900

    fix: 프론트엔드 요청 메서드 오류 수정

--- common/snowflake/src/test/java/kuke/board/common/snowflake/SnowflakeTest.java
+++ common/snowflake/src/test/java/kuke/board/common/snowflake/SnowflakeTest.java
@@ -10,7 +10,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
 class SnowflakeTest {
 	Snowflake snowflake = new Snowflake();
