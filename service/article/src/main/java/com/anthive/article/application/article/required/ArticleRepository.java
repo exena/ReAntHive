4245d47b40c1836afc966990b1b0912939150511
exena <exena@naver.com>
2026-01-11 17:39:41 +0900

    feat: 게시글 목록 페이지 조회 쿼리 최적화

--- service/article/src/main/java/com/anthive/article/application/article/required/ArticleRepository.java
+++ service/article/src/main/java/com/anthive/article/application/article/required/ArticleRepository.java
@@ -5,8 +5,26 @@ import com.anthive.article.domain.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Page<Article> findByMember(Member member, Pageable pageable);
+
+    @Query(
+            value = "select article.id, article.title, article.content, article.board_id, article.member_id, " +
+                    "article.created_at, article.modified_at " +
+                    "from (" +
+                    "   select id from article " +
+                    "   where board_id = :boardId " +
+                    "   order by id desc " +
+                    "   limit :limit offset :offset " +
+                    ") t left join article on t.id = article.id ",
+            nativeQuery = true
+    )
+    List<Article> findAll(@Param("boardId") Long boardId, @Param("offset") Long offset, @Param("limit") Long limit);
+
 }
